690f0fd3f2689a9628ffcf8416fd15ae4e7e4524
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-09-04 13:58:59 +0200

    Mentorat 4/9/24

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,65 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { screen, waitFor } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import mockStore from "../__mocks__/store.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import router from "../app/Router.js";
+import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
+import userEvent from "@testing-library/user-event";
 
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      const windowIcon = screen.getByTestId("icon-mail");
+      expect(windowIcon.getAttribute("class")).toBe("active-icon");
+    });
+  });
+  describe("When I select a file to transfer", () => {
+    test("Input file length should show be equal to 1", async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "a@a",
+        })
+      );
+      document.body.innerHTML = NewBillUI();
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage,
+      });
+
+      const file = new File(["hello"], "hello.png", { type: "image/png" });
+      const handleFileInputChange = jest.fn((e) => newBill.handleChangeFile(e));
+      await waitFor(() => screen.getByTestId("file"));
+      const inputFile = screen.getByTestId("file");
+      inputFile.addEventListener("change", handleFileInputChange);
+      userEvent.upload(handleFileInputChange, file);
+      expect(inputFile.files).toHaveLength(1);
+    });
+  });
+});
